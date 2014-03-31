b0e2e3d4144c71a2de4117b98c102e9e56363f60
FunThomas424242 <funthomas424242@gmail.com>
2014-03-31 23:13:01 +0200

    JPA Preferences added
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/main/java/dbpreferences/plugin/preferences/PreferenceConstants.java
+++ src/main/java/dbpreferences/plugin/preferences/PreferenceConstants.java
@@ -9,8 +9,16 @@ public class PreferenceConstants {
 
 	public static final String P_BOOLEAN = "booleanPreference";
 
-	public static final String P_CHOICE = "choicePreference";
-
-	public static final String P_STRING = "stringPreference";
+	
+	
+	public static final String P_JPA_UNITNAME = "jpaUnitNamePreference";
+	public static final String P_USERNAME = "userNamePreference";
+	public static final String P_PASSWORD = "passwordPreference";
+	public static final String P_DRIVERCLASS = "driverClassPreference";
+	public static final String P_URL = "urlPreference";
+	public static final String P_LOGLEVEL = "logLevelPreference";
+	public static final String P_WAEVING_INTERNAL = "waevingPreference";
+	public static final String P_DDL_GENERATION = "ddlGenerationPreference";
+	public static final String P_DDL_GENERATION_MODE = "ddlGenerationModePreference";
 	
 }
